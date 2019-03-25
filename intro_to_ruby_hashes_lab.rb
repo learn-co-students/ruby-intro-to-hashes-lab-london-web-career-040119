c0744217f02426227f97e699dcf564544100f445
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-25 11:34:11 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,56 +1,55 @@
 def new_hash
-  {}
+  new_hash = {}
 end
 
 
 def actor
-  actor = {
-    :name => 'Dwayne The Rock Johnson'
-  }
+  actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 
 def monopoly
-	monopoly = {
-	  :railroads => {}
-	}
+	monopoly = {:railroads => {}}
 end
 
 
 def monopoly_with_second_tier
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
 	    :rent_in_dollars => {}
-	  }
+	  },
 	}
 end
 
 
 def monopoly_with_third_tier
-  monopoly = {
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+		monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {}, 
-        :pennsylvania_railroad => {}, 
-        :b_and_o_railroad => {}, 
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {}, 
         :shortline => {}
 	    },
 	    :rent_in_dollars => {
-	      one_piece_owned: 25, 
-        two_pieces_owned: 50, 
-        three_pieces_owned: 100, 
-        four_pieces_owned: 200
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
 	    }
-	  }
+	  },
 	}
-  
 end
 
+
 def monopoly_with_fourth_tier
-  monopoly = {
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+		monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
@@ -60,12 +59,11 @@ def monopoly_with_fourth_tier
         :shortline => {"mortgage_value" => "$800"}
 	    },
 	    :rent_in_dollars => {
-	      one_piece_owned: 25, 
-        two_pieces_owned: 50, 
-        three_pieces_owned: 100, 
-        four_pieces_owned: 200
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
 	    }
-	  }
+	  },
 	}
-
 end
